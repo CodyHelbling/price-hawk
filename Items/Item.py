0b54c4d23c60a9c8bf3fe672f913d9740b8e729e
Cody Helbling <helblingcody@gmail.com>
2017-01-17 16:25:38 -0800

    Added possible get and set methods as comments.

--- Items/Item.py
+++ Items/Item.py
@@ -6,8 +6,27 @@ This file contains the class for a PriceHawk item.
 class Item():
 
     name = 'Uninitialized'
-    price = 'Uninitialized'
+    original_price = 'Uninitialized'
+    current_price = 'Uninitialized'
+    url = 'Uninitialized'
     
-    def __init__(self, name, price):
+    def __init__(self, name, price, url):
         self.name = name
-        self.price = price
+        self.original_price = price
+        self.url = url
+
+    # def get_current_price()
+        
+    # def get_name()
+
+    # def get_original_price()
+
+    # def get_url()
+
+    # def get_users()?
+
+    # def set_current_price()
+
+    # def set_url()
+    
+    # def set_name()
