f0baf3fd7c5024afd42363bd7650c518dea8b994
Cody Helbling <helblingcody@gmail.com>
2017-01-18 11:11:54 -0800

    Added static file route.

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from bottle import route, run, request, response
+from bottle import static_file
 from Scraper.amazon_scraper import scraper
 
 
@@ -7,7 +8,22 @@ def add_url():
     url = request.query.url
     name = request.query.name
     price = scraper(url)
-    return "Hello " + name + ", is your url: " + str(url) + " and price: " + str(price) + "?"
+    return ("Hello " + name +
+            ", is your url: " + str(url) +
+            " and price: " + str(price) + "?")
+
+
+# Serve Static Files
+@route('/static/<filename>')
+def server_static(filename):
+    print "   Filename: ", filename
+    return static_file(filename, root='./Static/JavaScript')
+
+
+@route('/')
+def home():
+    response.content_type = 'text/html'
+    return static_file('home.html', root='./Static/HTML')
 
 
 run(host='localhost', port=8080, debug=True)
