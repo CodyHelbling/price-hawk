c9a627ebf5439aeff33982056257a089a4913cd3
Cody Helbling <helblingcody@gmail.com>
2017-01-17 17:21:27 -0800

    Added amazon specific function to scraper and git rm~ed deleted files.

--- lxml_scraper/amazon_scraper.py
+++ lxml_scraper/amazon_scraper.py
@@ -1,7 +1,22 @@
 from lxml import html
 import requests
 
-page = requests.get("https://www.amazon.com/Dell-210-ADRZ-DELL-UltraSharp-UP2715K/dp/B00R420SU4/ref=sr_1_9?s=pc&ie=UTF8&qid=1484685159&sr=1-9&keywords=monitor&refinements=p_n_feature_three_browse-bin%3A12659079011")
-tree = html.fromstring(page.content)
 
-print tree.xpath('//span[@id="priceblock_ourprice"]/text()')
+def test_scraper():
+    page = requests.get("https://www.amazon.com/Dell-210-ADRZ-DELL-UltraSharp-UP2715K/dp/B00R420SU4/ref=sr_1_9?s=pc&ie=UTF8&qid=1484685159&sr=1-9&keywords=monitor&refinements=p_n_feature_three_browse-bin%3A12659079011")
+    tree = html.fromstring(page.content)
+    print tree.xpath('//span[@id="priceblock_ourprice"]/text()')
+
+
+def scraper(url):
+    print "Scraper URL: ", url
+    page = requests.get(url)
+    tree = html.fromstring(page.content)
+    price = tree.xpath('//span[@id="priceblock_ourprice"]/text()')
+    print "Scraper Price: ", price
+    
+    return price
+
+
+if __name__ == "__main__":
+    test_scraper()
