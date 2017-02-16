7b445a5b2f9103f4c6b98ae9683dfa1e15d9e33c
Cody Helbling <helblingcody@gmail.com>
2017-02-15 21:52:37 -0800

    Able to find prices on page.

--- test.py
+++ test.py
@@ -1,9 +1,13 @@
-import BeautifulSoup
+import re
+
 import gumbo
 import requests
 from gumbo import soup_adapter
 
-url = ('https://docs.python.org/2/tutorial/errors.html')
+from bs4 import BeautifulSoup
+
+#url = ('https://www.amazon.com/s/ref=lp_679307011_ex_n_3?rh=n%3A7141123011%2Cn%3A7147441011%2Cn%3A679255011&bbn=7147441011&sort=date-desc-rank&ie=UTF8&qid=1487221728&lo=fashion')
+url = ('https://www.amazon.com/Supra-Henry-stealth-brilliant-fullgrain/dp/B0050NGEAW/ref=sr_1_7?s=apparel&ie=UTF8&qid=1487221744&sr=1-7&nodeID=679255011')
 response = requests.get(url)
 print response.status_code
 # print response.headers['content-type']
@@ -12,8 +16,9 @@ s = (response.content).decode("utf8")
 # print "res: ", response
 soup = gumbo.soup_parse(s)
 
+prices = []
 for node in soup.findAll(text=True):
-    """
-    \$?[0-9]+\.[0-9]+|\$?[0-9]+
-    """
-    print node, "\n\n"
+    m = re.search('\$?[0-9]+\.[0-9][0-9]|\$[0-9]+', node)
+    if m:
+        print "Regex match: ", m.group(0)
+        # print node, "\n"
