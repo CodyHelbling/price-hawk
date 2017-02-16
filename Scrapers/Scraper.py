5310927294fce0bf0879ca737e9d1826282c2fa6
Cody Helbling <helblingcody@gmail.com>
2017-02-15 20:56:14 -0800

    Added Scraper file.

---
+++ Scrapers/Scraper.py
@@ -0,0 +1,38 @@
+"""
+This file contains the class for a PriceHawk Scraper.
+
+!!! NONFUNCTIONAL INCOMPLETE IMPLEMENTATION !!!
+
+"""
+
+
+class Scraper():
+
+    name = 'Uninitialized'
+    original_price = 'Uninitialized'
+    current_price = 'Uninitialized'
+    url = 'Uninitialized'
+    
+    def __init__(self, name, price, url):
+        self.name = name
+        self.original_price = price
+        self.current_price = price
+        self.url = url
+
+    def get_current_price(self):
+        return self.current_price
+
+    # def get_name()
+
+    # def get_original_price()
+
+    # def get_url()
+
+    # def get_users()?
+
+    # def set_current_price()
+
+    # def set_url()
+    
+    # def set_name()
+  
