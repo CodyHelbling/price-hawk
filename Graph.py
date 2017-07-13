8c1b6923ee8e564a8e551f505286737afa7d18f2
Cody Helbling <codyhelbling@gmail.com>
2017-07-12 22:28:02 -0600

    Added Item and Store graph functions.

--- Graph.py
+++ Graph.py
@@ -2,9 +2,13 @@ from neo4j.v1 import GraphDatabase
 
 driver = GraphDatabase.driver("bolt://localhost:7687", auth=("neo4j", "sharpie7"))
 
-id = 0
 
-# Add User
+user_id_counter = 0
+item_id_counter = 0
+store_id_counter = 0
+
+
+# -------------- User ---------------
 def add_user(tx, name, id):
     tx.run("CREATE (n: User { "
            "name: $name,"
@@ -14,45 +18,79 @@ def add_user(tx, name, id):
            id=id
     )
 
-# Test User Was Added
-# Was User Removed
-def find_user(tx, name):
-    result = tx.run("MATCH (n: User {name: $name})"
-                     "RETURN n.name as name, n.id as id",
-                     name=name)
-
-    for record in result:
-        print record["name"] + " " +  str(record["id"])
-
+    
 def find_user(tx, id):
     result = tx.run("MATCH (n: User {id: $id})"
-                     "RETURN n.name as name, n.id as id",
-                     id=id)
-
+                    "RETURN n.name as name, n.id as id",
+                    id=id)
     for record in result:
         print record["name"] + " " +  str(record["id"])
 
-# Remove User
+
 def delete_user(tx, id):
-    tx.run("MATCH (n:User {id: $id})"
-           "DETACH DELETE n",
-           id=id)
+    result = tx.run("MATCH (n:User {id: $id})"
+                    "DETACH DELETE n",
+                    id=id)
+    if result.single() == None:
+        return True
+    else:
+        return False
 
-    result = driver.session().find_user(id)
     
+# ------------- Item ------------
+    
+def add_item(tx, name, id):
+    result = tx.run("CREATE (n :Item {"
+                    "name: $name,"
+                    "id: $id"
+                    "})",
+                    name=name,
+                    id=id)
+
+def find_item(tx, id):
+    result = tx.run("MATCH (n:Item {id: $id})"
+                    "RETURN n.name as name, n.id as id",
+                    id=id)
+    
+    
+def delete_item(tx, id):
+    result = tx.run("MATCH (n:Item {id: $id})"
+                    "DETACH DELETE n",
+                    id=id)
+    if result.single() == None:
+        return True
+    else:
+        return
 
     
-# Add Item
-def add_item(tx, name):
-    tx.run("CREATE (n :Item { name: $name })", name=name)
+# ----------- Store -----------
+
+def add_store(tx, name, id):
+    result = tx.run("MATCH(n:Store {"
+                    "name: $name,"
+                    "id: $id"
+                    "})",
+                    name=name,
+                    id=id)
+
+def find_store(tx, id):
+    result = tx.run("MATCH (n:Store {id: $id}"
+                    "RETURN n.name as name, n.id as id",
+                    id=id)
+
+def delete_store(tx, id):
+    result = tx.run("MATCH (n:Store {id: $id}"
+                    "DETACH DELETE n",
+                    id=id)
+    if result.single() == None:
+        return True
+    else:
+        return False
+
     
-# Was Item Added
-# Remove Item
-# Was Item Removed
-# Add Store
-# Was Store Added
-# Remove Store
-# Was Store Removed
+# --------- Follows -----------
+
+
 # Add User Follows Item
 # Was Follows Added
 # Remove User Follows Item
@@ -71,8 +109,8 @@ def print_users(tx, name):
 
         
 with driver.session() as session:
-    session.write_transaction(add_user, "Test User", id)
-    id += 1
-    session.write_transaction(find_user, "Test User")
+    session.write_transaction(add_user, "Test User", user_id_counter)
+    user_id_counter += 1
+    session.write_transaction(find_user, 0)
     session.write_transaction(delete_user, 0)
-    session.write_transaction(find_user, "Test User")
+    session.write_transaction(find_user, 0)
